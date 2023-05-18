6c596a45473860868814a1cfc808faa1275029bf
Luigi Caravelli <luigi.caravelli98@gmail.com>
2023-05-18 12:35:05 +0200

    db identity

---
+++ FotoAlbum/PhotoAlbumContext.cs
@@ -0,0 +1,29 @@
+﻿using FotoAlbum.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace FotoAlbum
+{
+    public class PhotoAlbumContext : IdentityDbContext<ApplicationUser>
+    {
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+        public DbSet<ImageEntry> ImageEntries { get; set; }
+        public DbSet<Photo> Photos { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Message> Messages { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=localhost;Initial Catalog=FotoAlbum;Integrated Security=True;TrustServerCertificate=True");
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+
+        public PhotoAlbumContext() : base() { }
+
+        public PhotoAlbumContext(DbContextOptions<PhotoAlbumContext> dbContextOptions) : base(dbContextOptions) { }
+    }
+}
